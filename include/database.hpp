b0a0db90b2f2e36eac879f2fdb2f87526d86c9a5
Chent <bz20100405@163.com>
2017-08-15 17:46:58 +0800

    update printer.hpp and database.hpp Be careful, APIs have been changed!

--- include/database.hpp
+++ include/database.hpp
@@ -1,7 +1,11 @@
 #ifndef DATABASE_HPP
 #define DATABASE_HPP
 
+#include <stdio.h>
 #include "defines.hpp"
+#include <string>
+#include "sqlite3.h" 
+using namespace std;
 
 class database{
 public:
@@ -9,10 +13,16 @@ public:
 
 	~database();
 
-	void insert(form& m_form);   //save new form
+	void new_table(string table_name);            //create table in the first time
+
+	void insert(form& m_form, string table_name);   //save new form  table_name should be English
 
 	void check(form& m_form, string id);		 //check old form with id
-	
+
+private:
+   sqlite3 *db;
+   char *zErrMsg;
+   int rc;	
 };
 
 #endif
