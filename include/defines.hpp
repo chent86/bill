35f10f3a47c2501f21bb13f6177b199a61f0cc0d
Chent <bz20100405@163.com>
2017-08-15 17:41:11 +0800

    change defines.hpp to correct label

--- include/defines.hpp
+++ include/defines.hpp
@@ -12,25 +12,27 @@ struct form {
 	};
 
 	struct Person {
-		string name;		 //姓名
+		string company;		 //公司
+		string landline_telephone;    //固定电话
 		string address;   //单位地址
-		string telephone;    //电话
+		string telephone;    //手机
 	};
   
 	struct Cargo{
+		string number;     //货物件数
 		string name;       //货物名称
 		string type;       //包装类型
-		string number;     //货物件数
-		string unit;       //单位
-		string weight;     //重量/体积
-		string price;      //声明/价格
+		string weight;     //重量
+		string volume;     //体积
+		string price;      //货保价值
 	};
 
 	struct Expense{
 		string freight;    //运费
-		string shuttle;    //接送费
-		string insurance;  //保险费
+		string shuttle;    //送货费
 		string others;     //其他费用
+		string pick_up;    //提货费
+		string insurance;  //保险费
 		string total;      //合计
 	};
 
@@ -48,19 +50,22 @@ struct form {
 
 	Date date;			 //日期
 
-	Person sender;       //发货人
+	Person receiver;       //收货人
 
-	Person receiver;     //收货人
+	Person sender;     //发货人
 
-	Cargo m_cargos[3];   //货物信息
+	Cargo cargo;   //货物信息
+
+	string note;         //备注
 
 	Expense expense;	 //费用（元）
 
-	string payment_method;  //运费交付方式
-
 	Total_payment pay;   //运费合计
 
-	string note;         //备注
+	string document_maker;  //制单人
+
+	string consignee;     //提货人
+
 };
 
 #endif
